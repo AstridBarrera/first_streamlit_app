c392800e9ac7df02394dc14a581b80945550df87
AstridBarrera <137447550+AstridBarrera@users.noreply.github.com>
2023-06-23 08:52:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner by Astrid :)')
+streamlit.title('My Parents New Healthy Diner - by Astrid :)')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
