94f002180f4b402dc5820c53b9b5f0dc8445dbe9
AstridBarrera <137447550+AstridBarrera@users.noreply.github.com>
2023-06-30 15:43:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ try:
   fruit_choice = sl.text_input('What fruit would you like information about?', 'Kiwi')
   if not fruit_choice:
     sl.error("Please select a fruit to get information")
-  else
+  else:
      # New section to make connection with the webpage where the data for fruits is
      # import requests
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
